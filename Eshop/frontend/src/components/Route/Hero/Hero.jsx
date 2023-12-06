682bb9173ba7a4cce9d02ad20541d4631d943265
Shanthinath Shedabale <shanthinathshedabale17@gmail.com>
2023-12-06 17:58:44 +0530

    first commit

---
+++ Eshop/frontend/src/components/Route/Hero/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../../styles/styles";
+
+const Hero = () => {
+  return (
+    <div
+      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
+      style={{
+        backgroundImage:
+          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
+      }}
+    >
+      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
+        <h1
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+        >
+          Best Collection for <br /> home Decoration
+        </h1>
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
+        Welcome to our electronics shop, where technology meets your needs! <br /> We offer a wide range of cutting-edge gadgets, smart home devices. <br /> Our knowledgeable team is here to help you find the perfect products at affordable prices. <br /> Shop with us and experience a great today!
+        </p>
+        <Link to="/products" className="inline-block">
+            <div className={`${styles.button} mt-5`}>
+                 <span className="text-[#fff] font-[Poppins] text-[18px]">
+                    Shop Now
+                 </span>
+            </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ Eshop/frontend/src/routes/ShopRoutes.js
@@ -0,0 +1,23 @@
+import ShopDashboardPage from "../pages/Shop/ShopDashboardPage";
+import ShopCreateProduct from "../pages/Shop/ShopCreateProduct";
+import ShopAllProducts from "../pages/Shop/ShopAllProducts";
+import ShopAllCoupouns from "../pages/Shop/ShopAllCoupouns";
+import ShopPreviewPage from "../pages/Shop/ShopPreviewPage";
+import ShopAllOrders from "../pages/Shop/ShopAllOrders";
+import ShopOrderDetails from "../pages/Shop/ShopOrderDetails";
+import ShopAllRefunds from "../pages/Shop/ShopAllRefunds";
+import ShopSettingsPage from "../pages/Shop/ShopSettingsPage";
+import ShopInboxPage from "../pages/Shop/ShopInboxPage";
+
+export {
+    ShopDashboardPage,
+    ShopCreateProduct,
+    ShopAllProducts,
+    ShopAllCoupouns,
+    ShopPreviewPage,
+    ShopAllOrders,
+    ShopOrderDetails,
+    ShopAllRefunds,
+    ShopSettingsPage,
+    ShopInboxPage
+}
